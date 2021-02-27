e8994f91c88eb2a181e7d1930795f930f403cd5c
Butcat <alexis2morin@gmail.com>
2021-02-27 10:00:17 +0100

    answer to ping by pong

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
-package main 
+package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 
 	// internal pkg
-	"bug-tracker/config"
+	"golady-tracker/config"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -18,6 +21,31 @@ func main() {
 	discord, err := discordgo.New("Bot " + config.Token)
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
+
+	// Register the messageCreate func as a callback for MessageCreate events.
+	discord.AddHandler(messageCreate)
+
+	// Open a websocket connection to Discord and begin listening.
+	err = discord.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	// Cleanly close down the Discord session.
 	discord.Close()
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Content == "ping" {
+		s.ChannelMessageSend(m.ChannelID, "Pong!")
+	}
 }
